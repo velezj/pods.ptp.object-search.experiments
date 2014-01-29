fdf2381852a20b8aa12e23e1cbcdeddcb4d36ba2
javier <velezj@alum.mit.edu>
2014-01-29 10:18:33 -0500

    trying different intesity_estimates in test

--- test/ruler-process-intensity.cpp
+++ test/ruler-process-intensity.cpp
@@ -2,6 +2,7 @@
 #include <p2l-rawseeds-experiments/register.hpp>
 #include <point-process-experiment-core/experiment_utils.hpp>
 #include <point-process-experiment-core/experiment_runner.hpp>
+#include <probability-core/distribution_utils.hpp>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -12,6 +13,7 @@
 using namespace math_core;
 using namespace point_process_experiment_core;
 using namespace point_process_core;
+using namespace probability_core;
 
 
 int main( int argc, char** argv )
@@ -21,14 +23,18 @@ int main( int argc, char** argv )
 
 
   // get the world to test
-  std::vector<nd_point_t> groundtruth = groundtruth_for_world( "rawseeds::biccoca_2009_02_27a" );
-  nd_aabox_t window = window_for_world( "rawseeds::biccoca_2009_02_27a" );
+  std::string data_id = "rawseeds::small_001";
+  std::vector<nd_point_t> groundtruth = groundtruth_for_world( data_id );
+  nd_aabox_t window = window_for_world( data_id );
+
+  std::cout << "data: " << data_id << "   " << groundtruth.size() << " points in " << window << std::endl;
+
 
   std::vector<std::string> models{ 
     "rawseeds::igmm_2d_weak_001",
-      "rawseeds::ruler_2d_mean_006",
-      "rawseeds::ruler_2d_mean_007",
-      "rawseeds::ruler_2d_mean_008",
+      // "rawseeds::ruler_2d_mean_006",
+      // "rawseeds::ruler_2d_mean_007",
+      // "rawseeds::ruler_2d_mean_008",
       };
   
   // test all known models
@@ -37,19 +43,21 @@ int main( int argc, char** argv )
     std::cout << "testing model: " << model_id << std::endl;
 
     // get the mode to test
-    boost::shared_ptr<mcmc_point_process_t> model = get_model_by_id( model_id, window,groundtruth );
+    boost::shared_ptr<mcmc_point_process_t> model = get_model_by_id( model_id, window, groundtruth );
 
     // ok, now add some observations to the model,
-    // all of the ground truth and all empty regions
+    // all of the ground truth and some empty regions
     double model_res = 10.0;
     marked_grid_t<bool> grid = point_set_as_grid( groundtruth, window, model_res );
+    std::cout << "marked cells: " << grid.all_marked_cells().size() << " / " << grid.all_cells().size() << std::endl;
     model->add_observations( groundtruth );
     size_t num_neg_obs = 0;
     size_t possible_neg_obs = 0;
     for( auto cell : grid.all_cells() ) {
-      if( (*grid( cell )) == true ) {
+      if( *grid(cell) &&
+	  *grid(cell) == true ) {
       } else {
-	if( rand() % 10 == 1) {
+	if( flip_coin( 0.1 ) ) {
 	  num_neg_obs++;
 	  model->add_negative_observation( grid.region( cell ) );
 	}
@@ -64,8 +72,10 @@ int main( int argc, char** argv )
     oss << "intensity-" << model_id << ".png";
     std::cout << "  ... creating intensity estimate  ..." << std::endl;
     histogram_t<double> intensity_estimate = model->intensity_estimate( window,
-									200,
-									100);
+									10,
+									10,
+									1,
+									true);
     
     save_png( oss.str(), intensity_estimate );
     std::cout << "created intensity estimate: " << oss.str() << std::endl;
